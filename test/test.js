9413d37309955b2e99b02fd7a290e0d9e4911707
YOUR NAME <erinpagemd@yahoo.com>
2015-01-23 11:34:52 -0600

    writing test, bringing in linting files

---
+++ test/test.js
@@ -0,0 +1,10 @@
+/* global describe, it */
+'use strict';
+
+var assert = require('assert');
+
+describe('Mocha', function () {
+  it('should assert true', function () {
+    assert(true);
+  });
+});
