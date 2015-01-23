6febdbe6a306d1b9128456cdd437813373c5ec87
YOUR NAME <erinpagemd@yahoo.com>
2015-01-23 13:30:02 -0600

    finished up some test stuff

--- test/test.js
+++ test/test.js
@@ -1,10 +1,12 @@
 /* global describe, it */
+/* jshint expr: true */
 'use strict';
 
-var assert = require('assert');
+var expect = require('chai').expect;
 
 describe('Mocha', function () {
-  it('should assert true', function () {
-    assert(true);
+  it('should expect true and false', function () {
+    expect(true).to.be.true;
+    expect(false).to.be.false;
   });
 });
