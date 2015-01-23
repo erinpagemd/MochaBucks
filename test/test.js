2e138f8e597130a6785e9cdd97eddd0e5c355697
YOUR NAME <erinpagemd@yahoo.com>
2015-01-23 14:22:23 -0600

    added a new test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,8 @@
 /* jshint expr: true */
 'use strict';
 
-var expect = require('chai').expect;
+var expect      = require('chai').expect,
+    Constructor = require(process.cwd() + '/lib/Constructor');
 
 describe('Mocha', function () {
   it('should expect true and false', function () {
@@ -10,3 +11,18 @@ describe('Mocha', function () {
     expect(false).to.be.false;
   });
 });
+
+describe('Constructor', function () {
+  describe('init', function () {
+    it('should have public variables', function () {
+      var constructed = new Constructor();
+      expect(constructed.publicVariable).to.equal('foo');
+    });
+    it('should accept an arguent', function () {
+      var constructed = new Constructor('bar');
+      expect(constructed.publicVariable).to.equal('bar');
+    })
+  });
+});
+
+
